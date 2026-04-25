563d44bbef3a79cb0061abdd1b28421d78f7e645
MariamElsoufyy <mariamelsoufyx@gmail.com>
2026-04-26 00:07:36 +0300

    dump

--- helpers/microphone_test.py
+++ helpers/microphone_test.py
@@ -14,8 +14,8 @@ import soundfile as sf
 import websockets
 
 
-WS_URL = "ws://34.147.147.165:8000/ws/voice-chat"
-#WS_URL = "ws://127.0.0.1:8000/ws/voice-chat"
+#WS_URL = "ws://34.147.147.165:8000/ws/voice-chat"
+WS_URL = "ws://127.0.0.1:8000/ws/voice-chat"
 
 TEST_FILES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_files")
 
